9c020ee9fb38305b4e907eaf6f1e856d9a272ce0
igoradamenko <mail@igoradamenko.com>
2018-08-20 14:01:19 +0300

    Move global variables for tests into the tests' config

--- .eslintrc.tests.js
+++ .eslintrc.tests.js
@@ -1,5 +1,16 @@
 module.exports = {
   extends: './index.js',
+  globals: {
+    afterEach: true,
+    before: true,
+    beforeEach: true,
+    browser: true,
+    casper: true,
+    describe: true,
+    expect: true,
+    it: true,
+    xit: true,
+  },
   plugins: ['funbox-rules'],
   rules: {
     'funbox-rules/no-only': 2,
