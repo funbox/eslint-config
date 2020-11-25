d299cac689a1fd174e781557413a77eb17563f73
igoradamenko <mail@igoradamenko.com>
2020-11-25 15:44:52 +0200

    Allow for..of loops to use

--- .eslintrc.js
+++ .eslintrc.js
@@ -53,6 +53,24 @@ module.exports = {
     // So leave the rule, but add an exception.
     'no-return-assign': ['error', 'except-parens'],
 
+    // Same as Airbnb used, but without restricting `ForOfStatement`.
+    // It's disabled there because it's “heavyweight” due to requiring regenerator-runtime. But our projects already use regenerator-runtime, so why not.
+    'no-restricted-syntax': [
+      'error',
+      {
+        selector: 'ForInStatement',
+        message: 'for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.',
+      },
+      {
+        selector: 'LabeledStatement',
+        message: 'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.',
+      },
+      {
+        selector: 'WithStatement',
+        message: '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.',
+      },
+    ],
+
     // Allow to use funcs and classes before their definition, 'cause it may be handy.
     // Such variables usage isn't allowed by default.
     'no-use-before-define': ['error', {
